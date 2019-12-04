4e4a9fb73e189adf44059dbb9dfd63a927915e4d
gmarupilla <marupilla@uchc.edu>
2019-12-04 11:35:41 -0500

    Update Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -4,8 +4,9 @@ FROM ubuntu:latest
 RUN apt update && apt install python3 python3-pip python3-dev -y
 # RUN apt update && apt add --no-cache python3-dev
 WORKDIR /app
-COPY copasi_api ./
+COPY requirements.txt ./
 RUN pip3 install --no-cache-dir -r requirements.txt
+COPY copasi_api ./
 ARG filename
 
 ENTRYPOINT [ "python3",  "simulation.py"]
--- copasi_api/simulation.py
+++ copasi_api/simulation.py
@@ -15,17 +15,14 @@
 # All rights reserved. 
 
 
-
-
-
-
 # This is an example on how to import an sbml file
 # create a report for a time course simulation 
 # and run a time course simulation
 # 
 
-from COPASI import *
 import sys
+
+from COPASI import *
 
 # create a datamodel
 try:
@@ -35,89 +32,89 @@ except:
 
 
 def main(args):
-  # the only argument to the main routine should be the name of an SBML file
-  if len(args) != 1:
-      sys.stderr.write("Usage: example3 SBMLFILE\n")
-      return 1;
+    # the only argument to the main routine should be the name of an SBML file
+    if len(args) != 1:
+        sys.stderr.write("Usage: example3 SBMLFILE\n")
+        return 1;
 
-  filename = args[0]
-  try:
-      # load the model
-      if  not dataModel.importSBML(filename):
-          print("Couldn't load {0}:".format(filename))
-          print(CCopasiMessage.getAllMessageText())
-  except:
-      sys.stderr.write("Error while importing the model from file named \"" + filename + "\".\n")
-      return 1
+    filename = args[0]
+    try:
+        # load the model
+        if not dataModel.importSBML(filename):
+            print("Couldn't load {0}:".format(filename))
+            print(CCopasiMessage.getAllMessageText())
+    except:
+        sys.stderr.write("Error while importing the model from file named \"" + filename + "\".\n")
+        return 1
 
-  model = dataModel.getModel()
-  assert model is not None
+    model = dataModel.getModel()
+    assert model is not None
 
-  # get the trajectory task object
-  trajectoryTask = dataModel.getTask("Time-Course")
-  assert (isinstance(trajectoryTask, CTrajectoryTask))
+    # get the trajectory task object
+    trajectoryTask = dataModel.getTask("Time-Course")
+    assert (isinstance(trajectoryTask, CTrajectoryTask))
 
-  # run a deterministic time course
-  trajectoryTask.setMethodType(CTaskEnum.Method_deterministic)
+    # run a deterministic time course
+    trajectoryTask.setMethodType(CTaskEnum.Method_deterministic)
 
-  # activate the task so that it will be run when the model is saved
-  # and passed to CopasiSE
-  trajectoryTask.setScheduled(True)
+    # activate the task so that it will be run when the model is saved
+    # and passed to CopasiSE
+    trajectoryTask.setScheduled(True)
 
-  # create a new report that captures the time course result
-  report = create_report(model)
-  # set the report for the task
-  trajectoryTask.getReport().setReportDefinition(report)
-  # set the output filename
-  trajectoryTask.getReport().setTarget("example3.txt")
-  # don't append output if the file exists, but overwrite the file
-  trajectoryTask.getReport().setAppend(False)
+    # create a new report that captures the time course result
+    report = create_report(model)
+    # set the report for the task
+    trajectoryTask.getReport().setReportDefinition(report)
+    # set the output filename
+    trajectoryTask.getReport().setTarget("example3.txt")
+    # don't append output if the file exists, but overwrite the file
+    trajectoryTask.getReport().setAppend(False)
 
-  # get the problem for the task to set some parameters
-  problem = trajectoryTask.getProblem()
-  assert (isinstance(problem, CTrajectoryProblem))
+    # get the problem for the task to set some parameters
+    problem = trajectoryTask.getProblem()
+    assert (isinstance(problem, CTrajectoryProblem))
 
-  # simulate 100 steps
-  problem.setStepNumber(100)
-  # start at time 0
-  dataModel.getModel().setInitialTime(0.0)
-  # simulate a duration of 10 time units
-  problem.setDuration(10)
-  # tell the problem to actually generate time series data
-  problem.setTimeSeriesRequested(True)
-  # tell the problem, that we want exactly 100 simulation steps (not automatically controlled)
-  problem.setAutomaticStepSize(False)
-  # tell the problem, that we don't want additional output points for event assignments
-  problem.setOutputEvent(True)
+    # simulate 100 steps
+    problem.setStepNumber(100)
+    # start at time 0
+    dataModel.getModel().setInitialTime(0.0)
+    # simulate a duration of 10 time units
+    problem.setDuration(10)
+    # tell the problem to actually generate time series data
+    problem.setTimeSeriesRequested(True)
+    # tell the problem, that we want exactly 100 simulation steps (not automatically controlled)
+    problem.setAutomaticStepSize(False)
+    # tell the problem, that we don't want additional output points for event assignments
+    problem.setOutputEvent(True)
 
-  # set some parameters for the LSODA method through the method
-  method = trajectoryTask.getMethod()
+    # set some parameters for the LSODA method through the method
+    method = trajectoryTask.getMethod()
 
-  parameter = method.getParameter("Absolute Tolerance")
-  assert parameter is not None
-  assert parameter.getType() == CCopasiParameter.Type_UDOUBLE
-  parameter.setValue(1.0e-12)
+    parameter = method.getParameter("Absolute Tolerance")
+    assert parameter is not None
+    assert parameter.getType() == CCopasiParameter.Type_UDOUBLE
+    parameter.setValue(1.0e-12)
 
-  try:
-      # now we run the actual trajectory
-      result=trajectoryTask.process(True)
-  except:
-      sys.stderr.write("Error. Running the time course simulation failed.\n")
-      # check if there are additional error messages
-      if CCopasiMessage.size() > 0:
-          # print the messages in chronological order
-          sys.stderr.write(CCopasiMessage.getAllMessageText(True))
-      return 1
-  if not result:
-      sys.stderr.write("Error. Running the time course simulation failed.\n" )
-      # check if there are additional error messages
-      if CCopasiMessage.size() > 0:
-          # print the messages in chronological order
-          sys.stderr.write(CCopasiMessage.getAllMessageText(True))
-      return 1
+    try:
+        # now we run the actual trajectory
+        result = trajectoryTask.process(True)
+    except:
+        sys.stderr.write("Error. Running the time course simulation failed.\n")
+        # check if there are additional error messages
+        if CCopasiMessage.size() > 0:
+            # print the messages in chronological order
+            sys.stderr.write(CCopasiMessage.getAllMessageText(True))
+        return 1
+    if not result:
+        sys.stderr.write("Error. Running the time course simulation failed.\n")
+        # check if there are additional error messages
+        if CCopasiMessage.size() > 0:
+            # print the messages in chronological order
+            sys.stderr.write(CCopasiMessage.getAllMessageText(True))
+        return 1
 
-  # look at the timeseries
-  print_results(trajectoryTask)
+    # look at the timeseries
+    print_results(trajectoryTask)
 
 
 def print_results(trajectoryTask):
@@ -125,16 +122,16 @@ def print_results(trajectoryTask):
     # we simulated 100 steps, including the initial state, this should be
     # 101 step in the timeseries
     assert timeSeries.getRecordedSteps() == 101
-    print ("The time series consists of {0} steps.".format(timeSeries.getRecordedSteps()))
-    print ("Each step contains {0} variables.".format(timeSeries.getNumVariables()))
-    print ("\nThe final state is: ")
+    print("The time series consists of {0} steps.".format(timeSeries.getRecordedSteps()))
+    print("Each step contains {0} variables.".format(timeSeries.getNumVariables()))
+    print("\nThe final state is: ")
     iMax = timeSeries.getNumVariables()
     lastIndex = timeSeries.getRecordedSteps() - 1
     for i in range(0, iMax):
         # here we get the particle number (at least for the species)
         # the unit of the other variables may not be particle numbers
         # the concentration data can be acquired with getConcentrationData
-        print ("  {0}: {1}".format(timeSeries.getTitle(i), timeSeries.getData(lastIndex, i)))
+        print("  {0}: {1}".format(timeSeries.getTitle(i), timeSeries.getData(lastIndex, i)))
     # the CTimeSeries class now has some new methods to get all variable titles
     # as a python list (getTitles())
     # and methods to get the complete time course data for a certain variable based on
@@ -194,7 +191,4 @@ def create_report(model):
 
 
 if __name__ == '__main__':
-   main(sys.argv[1:]) 
-
-
-
+    main(sys.argv[1:])
