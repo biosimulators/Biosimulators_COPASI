9c564c0f4f4ee8d974c98335d9bd7ea22d611602
gmarupilla <marupilla@uchc.edu>
2020-01-29 14:49:12 -0500

    Complete config

--- src/config.py
+++ src/config.py
@@ -14,3 +14,9 @@ class Config:
     SEDML_DIR = 'usr/local/app/copasi/'
     JOBHOOK_URL = os.getenv('JOBHOOK_URL')
     JOB_ID = os.getenv('JOB_ID')
+    AUTH0_CLIENT_ID = os.getenv('AUTH0_CLIENT_ID')
+    AUTH0_CLIENT_SECRET = os.getenv('AUTH0_CLIENT_SECRET')
+    AUTH0_BIOSIMULATIONS_AUDIENCE = 'api.biosimulations.org'
+    AUTH0_TOKEN_URL = 'https://crbm.auth0.com/oauth/token'
+    AUTH0_GRANT_TYPE = 'client_credentials'
+
