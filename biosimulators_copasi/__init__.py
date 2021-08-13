3cf77c97ef515930c5794afad6d5fdb1c3628c28
Jonathan Karr <jonrkarr@gmail.com>
2021-08-13 16:34:56 -0400

    feat: added method to get the version of COPASI

--- biosimulators_copasi/__init__.py
+++ biosimulators_copasi/__init__.py
@@ -1,4 +1,15 @@
+import COPASI
+
 from ._version import __version__  # noqa: F401
 # :obj:`str`: version
 
 from .core import exec_sedml_docs_in_combine_archive  # noqa: F401
+
+
+def get_simulator_version():
+    """ Get the version of COPASI
+
+    Returns:
+        :obj:`str`: version
+    """
+    return COPASI.__version__
